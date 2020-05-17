f73586a4b6e5b9f28cd84235a3f512b5a0c7934c
Kamal Nanda <kamalnanda45@gmail.com>
2020-05-17 19:28:39 +0530

    Add files via upload

---
+++ precache-manifest.f3454b9e6dddef34a19e6ca7ce75397e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62037b1a70fb68d6461e90a629fad66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5799946ed542c1b6f85",
+    "url": "/static/css/main.572893d7.chunk.css"
+  },
+  {
+    "revision": "e8d9af5c05a929cf7276",
+    "url": "/static/js/2.ae89fd75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae89fd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5799946ed542c1b6f85",
+    "url": "/static/js/main.e34cfa71.chunk.js"
+  },
+  {
+    "revision": "2dec404b2a253fb26651",
+    "url": "/static/js/runtime-main.40d83138.js"
+  },
+  {
+    "revision": "acc83f311f9a17c603201bcef861a43d",
+    "url": "/static/media/close.acc83f31.png"
+  }
+]);
