0cd3303e72efc84aa4fc6c3f22817ed2f0d47793
Kamal Nanda <kamalnanda45@gmail.com>
2020-10-22 14:30:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Contact from './Contact/Contact'
 import FaqList from './faqList'
 function App() {
   return (
     <div className="App">
         <h1 className="header" align="center">FAQ</h1>
         <FaqList />
-        <footer>Developed By <a href = "https://kamalnanda.github.io" target="_blank">Kamal Nanda</a> in React</footer>
+        <Contact/>
+        <footer>Developed By <a href = "https://kamalnanda.github.io" rel="noopener noreferrer" target="_blank">Kamal Nanda</a> in React</footer>
     </div>
   );
 }
